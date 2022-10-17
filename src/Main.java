a71c37fd9fb495f8f92eebc77189922311136c99
a <yhma22.alkeal@folkuniversitetet.nu>
2022-10-17 12:46:19 +0200

    ny system out

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
         GameBoard gameBoard = new GameBoard();
         gameBoard.printGameBoard();
 
-
+        System.out.println("Alex branch");
         System.out.println("Viktor branch");
-        
+
     }
 }
