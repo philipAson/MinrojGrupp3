6ea69e13fec8f5ff11b3eb1ae9d5aa0ae0f51b22
a <yhma22.alkeal@folkuniversitetet.nu>
2022-10-17 15:30:49 +0200

    nya kommentarer på class

--- src/Cell.java
+++ src/Cell.java
@@ -1,2 +1,19 @@
 public class Cell {
+
+
+
+    // här ska vi göra så spelet visar vad som händer när du väljer en plats på GameBoard.
+
+    // exempel är att det kommer en siffra mellan 1-3 beroende på hur nära man är "bomben".
+
+    //Den ska också meddela om det är upptaget redan eller om man träffar en "bomb" på GameBoard.
+
+    //
+
+
+
+
+
+
+
 }
