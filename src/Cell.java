79ce319f7afea7de5716ecce7d6c17f3da0b9a13
Viktor <vjagebrant@gmail.com>
2022-10-17 19:48:43 +0200

    cell klass bas

--- src/Cell.java
+++ src/Cell.java
@@ -1,4 +1,37 @@
 public class Cell {
-    
 
+    private boolean bomb;
+    private int distance;
+    private boolean cleared;
+
+    public Cell(boolean bomb, int distance, boolean cleared) {
+        this.bomb = bomb;
+        this.distance = distance;
+        this.cleared = cleared;
+    }
+
+    public boolean isBomb() {
+        return bomb;
+    }
+
+    public void setBomb(boolean bomb) {
+        this.bomb = bomb;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public boolean isCleared() {
+        return cleared;
+    }
+
+    public void setCleared(boolean cleared) {
+        this.cleared = cleared;
+    }
+    
 }
