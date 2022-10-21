5502ab797d3e71083cb4a48b9497aa7d747ab4bf
Philip Andersson <113354376+philipAson@users.noreply.github.com>
2022-10-21 21:26:03 +0200

    Revert "Dynamic gameboard"

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         Game game = new Game(new Scanner(System.in));
-        System.out.println( "*============================*");
+
+        System.out.println("*============================*");
         System.out.println("*============================*");
         System.out.println("*==Whalecum in my Sweeper!==*");
         System.out.println("*============================*");
@@ -14,7 +15,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Player 1 enter name: ");
         String player = scanner.nextLine();
-
+        
         game.startGame();
 
 
